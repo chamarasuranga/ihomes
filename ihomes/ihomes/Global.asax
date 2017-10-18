562efee0967cf01fecc401be1912b3c613e9419e
chamara <mundigalac@dnb.com.au>
2017-10-18 16:03:00 +1100

    Initial commit

---
+++ ihomes/ihomes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ihomes.MvcApplication" Language="C#" %>
---
+++ ihomes/ihomes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ihomes.Startup))]
+namespace ihomes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
